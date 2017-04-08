51614974862e5d7c919820bea8a1a6772d059321
erika1mena <erika.mena@ucp.edu.co>
2017-04-07 20:04:15 -0500

    Add files via upload

---
+++ cine/script tbl_obra y tbl_genero.sql
@@ -0,0 +1,35 @@
+﻿-- Table: pelicula.tbl_genero
+
+-- DROP TABLE pelicula.tbl_genero;
+
+CREATE TABLE pelicula.tbl_genero
+(
+  id_genero character varying NOT NULL,
+  nombre_genero character varying,
+  descripcion_genero character varying,
+  id_obra character varying,
+  CONSTRAINT tbl_genero_pkey PRIMARY KEY (id_genero),
+  CONSTRAINT tbl_genero_id_obra_fkey FOREIGN KEY (id_obra)
+      REFERENCES pelicula.tbl_obra (id_obra) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE pelicula.tbl_genero
+  OWNER TO postgres;
+
+  
+CREATE TABLE pelicula.tbl_obra
+(
+  id_obra character varying NOT NULL,
+  nombre_obra character varying,
+  descripcion_obra character varying,
+  CONSTRAINT obra_pkey PRIMARY KEY (id_obra)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE pelicula.tbl_obra
+  OWNER TO postgres;
+  
